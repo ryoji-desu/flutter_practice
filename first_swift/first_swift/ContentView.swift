99ccac5f9e367bcdc123ef569f7dda7a1c47a682
ryouji nagai <ryoujinagai@CBcloudnoMacBook-Pro.local>
2020-09-06 22:41:16 +0900

    test

---
+++ first_swift/first_swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  first_swift
+//
+//  Created by ryouji nagai on 2020/09/06.
+//  Copyright © 2020 ryouji nagai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
